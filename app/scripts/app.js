51b91b49add8fcc28d1dbd93c94a78f5e99b2986
Babou <basile.soufiani@isep.fr>
2017-12-18 20:02:21 +0100

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name digitalisApp
+ * @description
+ * # digitalisApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('digitalisApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/Dashboard.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/nosTaches', {
+        templateUrl: 'views/nosTaches.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/acceuil', {
+        templateUrl: 'views/Acceuil.html',
+        controller: 'AcceuilCtrl',
+        controllerAs: 'acceuil'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
